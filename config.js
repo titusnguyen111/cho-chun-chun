2f4b5005bbb55f9d4516a0a6ee20e055c6547568
titusnguyen111 <huudat890@gmail.com>
2021-11-19 21:08:26 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'chó chun  ',
+    introDesc: `hãy trả lời câu hỏi dưới đây `,
+    btnIntro: 'oke',
+    title: 'hãy chọn số tiền cậu nợ tui ',
+    desc: 'lưu ý hãy trung thực  :> ',
+    btnYes: '500.000vnd ',
+    btnNo: '20.000vnd  ',
+    question:'mày có bị ngu không ',
+    btnReply: 'ok ',
+    reply: 'đúng t rất ngu  ',
+    mess: 'Tớ biết mà 🥰',
+    messDesc: 'nhớ ib tui để trả nợ  ',
+    btnAccept: 'Okiiiii lun <3',
+messLink: 'http://fb.com' //link mess của các bạn. 
+}
+
+    
